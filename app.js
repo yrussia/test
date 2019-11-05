85f7a676a18d7ecee105570df5adf0a6d6b608ab
EmelyanovKonstantin <EmelyanovKonstantin@yandex.ru>
2019-11-05 21:59:40 +0300

    First commit. Тестовое описание: создано приложение для чтения GeoJson

---
+++ app.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+let feature_reader = require('geojson-feature-reader')
+let read_stream = fs.createReadStream('Addressgeocoder.geojson')
+ 
+feature_reader(read_stream, feature_callback)
+    .then(count => console.log(`${count} features processed`))
+    .catch(e => console.error(`Unable to read data`))
+    
+function feature_callback(feature){
+    typeof feature === 'json' //true
+    
+    console.log(feature) 
+    // { "type": "Feature", "properties": { ... }, "geometry": { ... }
+    // or
+    // { "type": "feature", "properties": { ... }, "geometry": { ...} }
+}
+ 
